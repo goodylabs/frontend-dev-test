4813de82fe5dba3425f58f58f92e3a15a62f2198
Wiktor Pabich <pabichwiktor@gmail.com>
2019-04-25 16:08:30 +0200

    added a few BlockGrid and BlockGridManager unit tests

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
-import { Block, COLOURS } from '../app/javascript/grid';
-import { assert } from 'chai';
+import {Block, COLOURS, BlockGridManager, BlockGrid} from '../app/javascript/grid';
+import { assert, expect } from 'chai';
 
 let { describe, it } = window;
 
@@ -24,4 +24,35 @@ describe('Block', () => {
 
 describe('BlockGrid', () => {
 
+    it('should generate grid properly', () => {
+        const blockGrid = new BlockGrid();
+        const grid = blockGrid.grid;
+
+        assert.notEqual(grid, null, 'grid is not null');
+        grid.map(col => {
+            col.map(block => {
+                assert.instanceOf(block, Block, 'block is of type Block');
+            });
+        });
+    });
+});
+
+describe('BlockGridManager', () => {
+
+    it('should throw when calling deleteGroupByBlock method with no blockGrid binded', async () => {
+        const blockGridManager = new BlockGridManager(null);
+        const block = new Block(0,0);
+
+        await expect(blockGridManager.deleteGroupByBlock(block)).to.be.rejected;
+    });
+
+    it('should return proper ID when called _getBlockID method', () => {
+        const blockGrid = new BlockGrid();
+        const blockGridManager = new BlockGridManager(blockGrid);
+        const coords = {x: 1, y: 1};
+        const block = blockGrid.grid[coords.x][coords.y];
+
+        const expectedID = `block_${coords.x}x${coords.y}`;
+        assert.equal(blockGridManager._getBlockID(block), expectedID, 'getBlockID returned id as expected');
+    });
 });
