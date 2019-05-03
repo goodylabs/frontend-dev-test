d9eea496757fc4e9660ff2684ad179ae03e4aba5
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-05-03 12:59:25 +0200

    TESTS Update blockTest, moving columnElement to block constructor

--- test/blockTest.js
+++ test/blockTest.js
@@ -1,11 +1,12 @@
-import { Block } from '../app/javascript/block';
-import { COLOURS } from '../app/javascript/constants/colours';
-import { assert } from 'chai';
+import {Block} from '../app/javascript/block';
+import {COLOURS} from '../app/javascript/constants/colours';
+import {assert} from 'chai';
 
-let { describe, it } = window;
+let {describe, it} = window;
 
 describe('Block', () => {
-
+    const mockedBlockedClickedFunction = () => {
+    };
     it('should be created with correctly parameters', () => {
         let testCoords = [
             [1, 2],
@@ -26,17 +27,16 @@ describe('Block', () => {
         const colEl = document.createElement('div');
         colEl.className = 'col';
         colEl.id = id;
-        const mockedBlockedClickedFunction = () => {};
-        const block1 = new Block(0, 0);
-        const block2 = new Block(0, 1);
-        const block3 = new Block(0, 2);
+        const block1 = new Block(0, 0, colEl);
+        const block2 = new Block(0, 1, colEl);
+        const block3 = new Block(0, 2, colEl);
         const blocksArrays = [[block1], [block1, block2], [block1, block2, block3]];
         blocksArrays.forEach((blockArray) => {
             while (colEl.firstChild) {
                 colEl.removeChild(colEl.firstChild);
             }
             blockArray.forEach((block) => {
-                block.render(colEl, mockedBlockedClickedFunction);
+                block.render(mockedBlockedClickedFunction);
             });
             assert.equal(colEl.children.length, blockArray.length, 'block rendered correctly');
         });
