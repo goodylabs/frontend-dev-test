057845353fde5641adf595e69f5f9b378f05be75
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-05-03 13:00:21 +0200

    TESTS extracting counting child elements to new function, test if block is removed properly

--- test/blockColumnTest.js
+++ test/blockColumnTest.js
@@ -5,7 +5,20 @@ import {assert} from 'chai';
 let {describe, it} = window;
 
 describe('BlockColumn', () => {
-
+    const mockedBlockedClickedFunction = () => {};
+    const findChildElements = (colEl) => {
+        let removedHeightChildrenAmount = 0;
+        let blockChildrenAmount = 0;
+        for (let i = 0; i < colEl.children.length; i++) {
+            const children = colEl.children[i];
+            if (children.className === 'col-removed-space') {
+                removedHeightChildrenAmount++;
+            } else if (children.className === 'block') {
+                blockChildrenAmount++;
+            }
+        }
+        return {removedHeightChildrenAmount, blockChildrenAmount};
+    };
     it('should be created with correctly parameters', () => {
         let testXCoords = [0, 1, 2];
 
@@ -22,20 +35,9 @@ describe('BlockColumn', () => {
         const colEl = document.createElement('div');
         colEl.className = 'col';
         colEl.id = id;
-        const mockedBlockedClickedFunction = () => {
-        };
-        const column = new BlockColumn(0);
-        column.render(colEl, mockedBlockedClickedFunction);
-        let removedHeightChildrenAmount = 0;
-        let blockChildrenAmount = 0;
-        for (let i = 0; i < colEl.children.length; i++) {
-            const children = colEl.children[i];
-            if (children.className === 'col-removed-space') {
-                removedHeightChildrenAmount++;
-            } else if (children.className === 'block') {
-                blockChildrenAmount++;
-            }
-        }
+        const column = new BlockColumn(0, colEl);
+        column.render(mockedBlockedClickedFunction);
+        const {blockChildrenAmount, removedHeightChildrenAmount} = findChildElements(colEl);
         assert.equal(blockChildrenAmount, MAX_Y, 'blocks rendered correctly');
         assert.equal(removedHeightChildrenAmount, 1, 'removed height div rendered correctly');
     });
@@ -58,6 +60,23 @@ describe('BlockColumn', () => {
             const blockIndex = column.findBlockIndexByY(y);
             const blockByY = column.getBlockByY(y);
             assert.equal(column.getBlock(blockIndex).y, blockByY.y, 'correct block returned');
+        });
+    });
+
+    it('should remove block from column when contains column with y coordinate', () => {
+        const x = 0;
+        const yCoordinates = [0, 1, 2];
+        const colEl = document.createElement('div');
+        colEl.className = 'col';
+        colEl.id = 'col_' + x;
+        const column = new BlockColumn(x, colEl);
+        column.render(mockedBlockedClickedFunction);
+        yCoordinates.forEach((y, index) => {
+            column.removeBlockFromColumn(y);
+            const {blockChildrenAmount} = findChildElements(colEl);
+            assert.equal(column.removedBlocks, 1 + index, 'correct removedBlocks amount returned');
+            assert.equal(blockChildrenAmount, MAX_Y - 1 - index, 'correct blocks amount in document returned');
+            assert.equal(column.col.length, MAX_Y - 1 - index, 'correct blocks amount in object returned');
         });
     });
 });
