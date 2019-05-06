421a25c31b877ea2c36331a938c8279b61c7474e
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-05-06 20:34:59 +0200

    TESTS BlockGrid render test

--- test/blockGridTest.js
+++ test/blockGridTest.js
@@ -1,6 +1,6 @@
 import {assert} from 'chai';
 import {BlockGrid} from "../app/javascript/grid";
-import {MAX_X} from "../app/javascript/constants/gridSize";
+import {MAX_X, MAX_Y} from "../app/javascript/constants/gridSize";
 import {countChildElements} from './utils/countChildElements';
 
 let {describe, it} = window;
@@ -13,5 +13,16 @@ describe('BlockGrid', () => {
         const columnChildrenAmount = countChildElements(gridEl, 'col');
         assert.equal(grid.grid.length, MAX_X, 'grid has correct amount of columns');
         assert.equal(columnChildrenAmount, MAX_X, 'grid has correct amount of column elements');
+    });
+
+    it('should render block correctly', () => {
+        const gridEl = document.createElement('div');
+        gridEl.id = '#gridEl';
+        const grid = new BlockGrid(gridEl);
+        grid.render();
+        grid.grid.forEach((column) => {
+           const blockChildrenAmount = countChildElements(column.columnEl, 'block');
+            assert.equal(blockChildrenAmount, MAX_Y, 'grid has correct amount of blocks');
+        });
     });
 });
