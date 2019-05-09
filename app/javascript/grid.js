d3fd2b632f44fc20e765cf885e9b098bffce15f7
PawelHUAWEI <215675@edu.p.lodz.pl>
2019-05-09 16:58:22 +0200

    Hiding same color

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -8,6 +8,14 @@ export class Block {
         this.y = y;
         this.colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
     }
+    
+    changeBlockColorToWhite() {
+        this.colour = "white";
+    }
+
+    setCoulour(colour) {
+        this.colour = colour;
+    }
 }
 
 export class BlockGrid {
@@ -27,14 +35,14 @@ export class BlockGrid {
     }
 
     render (el = document.querySelector('#gridEl')) {
-        for (let x = 0; x < MAX_X; x++) {
+        for (var x = 0; x < MAX_X; x++) {
             let id = 'col_' + x;
             let colEl = document.createElement('div');
             colEl.className = 'col';
             colEl.id = id;
             el.appendChild(colEl);
 
-            for (let y = MAX_Y - 1; y >= 0; y--) {
+            for (var y = MAX_Y - 1; y >= 0; y--) {
                 let block = this.grid[x][y],
                     id = `block_${x}x${y}`,
                     blockEl = document.createElement('div');
@@ -42,7 +50,60 @@ export class BlockGrid {
                 blockEl.id = id;
                 blockEl.className = 'block';
                 blockEl.style.background = block.colour;
-                blockEl.addEventListener('click', (evt) => this.blockClicked(evt, block));
+
+                blockEl.addEventListener('click', (evt) => {
+                    let elementIdString = 'block_'+block.x+'x'+block.y;
+                    let clickedBlock = document.getElementById(elementIdString);
+                    
+                    var colourArray = new Array(MAX_X);
+                    for(let arrayCreateIter = 0; arrayCreateIter < MAX_Y; arrayCreateIter++)
+                        colourArray[arrayCreateIter] = new Array(MAX_Y);
+
+                    for(let test1 = 0; test1 < MAX_X; test1++) {
+                        for(let test2 = 0; test2 < MAX_Y; test2++) {
+                            colourArray[test1][test2] = false;
+                        }
+                    }
+
+                    for(let test1 = 0; test1 < MAX_X; test1++) {
+                        for(let test2 = MAX_Y-1; test2 >= 0; test2--) {
+                            if(test1 != x && test2 != y) {
+                                var elementIdStringTest = 'block_'+test1+'x'+test2;
+                                var blockTest = document.getElementById(elementIdStringTest);
+                                if(blockTest.style.background === clickedBlock.style.background)
+                                    colourArray[test1][test2] = true;
+                            }
+                        }
+                    }
+
+                    var colourArray2 = new Array(MAX_X);
+                    for(let arrayCreateIter = 0; arrayCreateIter < MAX_Y; arrayCreateIter++)
+                        colourArray2[arrayCreateIter] = new Array(MAX_Y);
+
+                    for(let test1 = 0; test1 < MAX_X; test1++) {
+                        for(let test2 = 0; test2 < MAX_Y; test2++) {
+                            colourArray2[test1][test2] = false;
+                        }
+                    }
+
+                    colourArray2[block.x][block.y] = true;
+
+                    findTest(colourArray, block.x, block.y, colourArray2);
+
+                    for(let test1 = 0; test1 < MAX_X; test1++) {
+                        for(let test2 = MAX_Y-1; test2 >= 0; test2--) {
+                            if(test1 != x && test2 != y) {
+                                var elementIdStringTest = 'block_'+test1+'x'+test2;
+                                var blockTest = document.getElementById(elementIdStringTest);
+                                if(colourArray2[test1][test2])
+                                    blockTest.style.background = "white";
+                            }
+                        }
+                    }
+
+                    this.blockClicked(evt, block, blockEl);
+                });
+
                 colEl.appendChild(blockEl);
             }
         }
@@ -50,9 +111,30 @@ export class BlockGrid {
         return this;
     }
 
-    blockClicked (e, block) {
-        console.log(e, block);
+    blockClicked (evt, block, blockEl) {
+        //console.log(evt, block);
+        block.changeBlockColorToWhite();
+        blockEl.style.background = "white";
     }
 }
 
 window.addEventListener('DOMContentLoaded', () => new BlockGrid().render());
+
+function findTest(colourArray, x, y, colourArray2) {
+    if(colourArray[x][y] == false) {
+        return;
+    }
+
+    if(colourArray[x][y] == true) {
+        colourArray[x][y] = false;
+        colourArray2[x][y] = true;
+        if(x+1 < MAX_X)
+            findTest(colourArray, x+1, y, colourArray2);
+        if(y+1 < MAX_Y)
+            findTest(colourArray, x, y+1, colourArray2);
+        if(x-1 >= 0)
+            findTest(colourArray, x-1, y, colourArray2);
+        if(y-1 >= 0)
+            findTest(colourArray, x, y-1, colourArray2);
+    }
+}
